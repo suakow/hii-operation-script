62b9dd601f6efd1006d6a4b830995a801740ecbe
Puri Phakmongkol <puri.pmk@gmail.com>
2022-07-09 02:20:18 +0700

    Create 1_update_indices.py

---
+++ 1_update_indices.py
@@ -0,0 +1,36 @@
+__author__ = "Puri Phakmongkol"
+__author_email__ = "me@puri.in.th"
+
+"""
+* HII Operation Script
+*
+* Created date : 05/07/2022
+*
++      o     +              o
+    +             o     +       +
+o          +
+    o  +           +        +
++        o     o       +        o
+-_-_-_-_-_-_-_,------,      o
+_-_-_-_-_-_-_-|   /\_/\
+-_-_-_-_-_-_-~|__( ^ .^)  +     +
+_-_-_-_-_-_-_-""  ""
++      o         o   +       o
+    +         +
+o      o  _-_-_-_- Update Indices
+    o           +
++      +     o        o      +
+""" 
+
+import pandas as pd
+import numpy as np
+
+# %config InlineBackend.figure_format = 'svg'
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+sns.set(rc={'figure.figsize':(6, 4)})
+
+import functools
+
