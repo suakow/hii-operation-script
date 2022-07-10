cf44c10870c56794c833eebac47218512fc0e68e
Puri Phakmongkol <puri.pmk@gmail.com>
2022-07-10 17:23:19 +0700

    Create 3_mm_midrange_modeling.py

---
+++ 3_mm_midrange_modeling.py
@@ -0,0 +1,34 @@
+__author__ = "Puri Phakmongkol"
+__author_email__ = "me@puri.in.th"
+
+"""
+* HII Operation Script
+*
+* Created date : 07/07/2022
+*
++      o     +              o
+    +             o     +       +
+o          +
+    o  +           +        +
++        o     o       +        o
+-_-_-_-_-_-_-_,------,      o
+_-_-_-_-_-_-_-|   /\_/\
+-_-_-_-_-_-_-~|__( ^ .^)  +     +
+_-_-_-_-_-_-_-""  ""
++      o         o   +       o
+    +         +
+o      o  _-_-_-_- Monsoon & MJO Mid Range Model
+    o           +
++      +     o        o      +
+""" 
+
+import pandas as pd
+import numpy as np
+
+# %config InlineBackend.figure_format = 'svg'
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+sns.set(rc={'figure.figsize':(6, 4)})
+sns.set(rc={'figure.dpi': 230})
