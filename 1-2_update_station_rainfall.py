79ec378c59ddd50cf79283ac2e301e260df72bc6
Puri Phakmongkol <puri.pmk@gmail.com>
2022-07-11 17:07:02 +0700

    Update 1-2_update_station_rainfall.py

--- 1-2_update_station_rainfall.py
+++ 1-2_update_station_rainfall.py
@@ -81,8 +81,64 @@ if __name__ == '__main__' :
     * Load Station Location
     """
     print('Load station locations...')
-    station_location_df = pd.read_excel(Path(project_path) / 'data' / 'static' / 'TMD-station-meta2021.xlsx')
+    station_location_df = pd.read_excel(Path(project_path) / 'data' / 'static' / config['station_location_file'])
     station_location_df = station_location_df[['code_tmd', 'name_tmd_en', 'main_basin', 'new_lat', 'new_long']]
     station_location_df['name_tmd_en'] = station_location_df['name_tmd_en'].apply(lambda _: _.replace('\t', ''))
 
-    print(station_location_df)
+    """
+    * Load rainfall observation files (Groundtruth)
+    """
+    monthly_path = Path(project_path) / 'data' / 'rainfall_map' / 'tiff_monthly'
+
+    year_list = []
+    month_list = []
+    for f in monthly_path.glob('*.tif') :
+        f_name = f.name
+        ym = f_name.split('o_th')[1].split('.tif')[0]
+        year_list.append(int(ym[:4]))
+        month_list.append(int(ym[4:]))
+
+    year_list = list(set(year_list))
+    monthlist = list(set(month_list))
+
+    year_list = [ str(_) for _ in year_list ]
+    # year_list = [ str(_) for _ in year_list if _ != '2021']
+    month_list = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12']
+
+    station_value_list = []
+    
+    for year in tqdm(year_list) :
+        for month in tqdm(month_list) : 
+            print(f'Y{year} M{month}')
+            in_f = monthly_path / f'o_th{year}{month}.tif'
+
+            if not(in_f.exists()) : 
+                continue
+
+            xa_sum = rioxarray.open_rasterio(in_f)
+            # xa_sum = xa_sum.drop_vars('band')
+
+            in_station_value = [datetime.datetime(int(year), int(month), 1), int(year), int(month)]
+
+            for station in tqdm(station_location_df['name_tmd_en'].values) :
+                # print(station)
+                # print(station_location_df[station_location_df['name_tmd_en'] == station]['new_long'][0])
+                station_value = xa_sum.sel(y=station_location_df[station_location_df['name_tmd_en'] == station]['new_lat'].values[0], 
+                                        x=station_location_df[station_location_df['name_tmd_en'] == station]['new_long'].values[0], 
+                                        method="nearest").values[0]
+                if station_value < 0.0001 :
+                    station_value = 0
+                
+                in_station_value.append(station_value)
+
+            station_value_list.append(in_station_value)
+
+    """
+    * End for Loop
+    """
+    station_value_df = pd.DataFrame(station_value_list, columns=['datetime', 'year', 'month'] + list(station_location_df['name_tmd_en'].values))
+    station_value_df.to_csv(Path(project_path) / 'data' / 'static' / config['station_rainfall_file'])
+
+    config['station_value_last_update'] = date_path
+    print(config)
+    open('%sconfig.yml'%(config['project_path']), 'w').write(yaml.dump(config))
